4439b3c0d1c277be45e4c7f87eed2dc2c47535ed
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-01-12 00:59:53 +0200

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,3 +15,15 @@ def hash_password(password: str) -> bytes:
     bPassword = password.encode()
     hashed = hashpw(bPassword, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
