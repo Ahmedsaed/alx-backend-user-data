20deb187f975fb7ff5329941c19a999028be3c44
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-01-17 00:03:29 +0200

    6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Definition of class BasicAuth
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Implement Basic Authorization protocol methods
+    """
+    pass
