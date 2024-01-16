c02399152e505a7f9911ce6b6afce0eca1ba7d95
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-01-16 23:57:09 +0200

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -43,9 +43,14 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns None
+        Returns the authorization header from a request object
         """
-        return None
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
